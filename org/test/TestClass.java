82708f5d6eaba5a81d98e91297209fec2bc04ffe
hemantharaj1 <hemantharaj1@gmail.com>
2018-07-24 10:06:43 +0530

    New Project

---
+++ org/test/TestClass.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestClass obj = new TestClass();
+		obj.test1();
+
+	}
+	
+	public void test1()
+	{
+		System.out.println("Test 1");
+	}
+
+}
